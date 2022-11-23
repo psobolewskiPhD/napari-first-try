18c950fe53d711f2bb35887a2e25dd99c7ea7bc5
Peter Sobolewski <pete.sd@gmail.com>
2022-11-23 21:59:40 +0100

    Add 2 sample data

--- src/napari_first_try/__init__.py
+++ src/napari_first_try/__init__.py
@@ -1,1 +1,3 @@
 __version__ = "0.0.1"
+
+from ._sample_data import faces_sample1, faces_sample2
---
+++ src/napari_first_try/_sample_data.py
@@ -0,0 +1,31 @@
+"""
+This module is an example of a barebones sample data provider for napari.
+It implements the "sample data" specification.
+see: https://napari.org/plugins/stable/npe2_manifest_specification.html
+Replace code below according to your needs.
+"""
+from __future__ import annotations
+
+from napari.types import LayerDataTuple
+
+
+
+def faces_sample1() -> LayerDataTuple:
+    from numpy.random import RandomState
+    from sklearn.datasets import fetch_olivetti_faces
+
+    rng = RandomState(0)
+
+    faces = fetch_olivetti_faces(return_X_y=False, shuffle=True, random_state=rng)
+
+    return [(faces.images, {"name": "Olivetti Faces"})]
+
+def faces_sample2() -> LayerDataTuple:
+    from numpy.random import RandomState
+    from sklearn.datasets import fetch_olivetti_faces
+
+    rng = RandomState(0)
+
+    faces = fetch_olivetti_faces(return_X_y=False, shuffle=True, random_state=rng)
+
+    return [(faces.images, {"name": "Olivetti Faces"})]
